8e8e10241be8782bbdf74655cbdec19460c691f4
DevoltionD6 <103224641+DevoltionD6@users.noreply.github.com>
2022-06-21 19:43:04 +0700

    Add files via upload

---
+++ asm2/ASM2/src/asm2/MyQueue.java
@@ -0,0 +1,48 @@
+package asm2;
+
+public class MyQueue<T> {
+
+    Node<T> head, tail;
+
+    public MyQueue() {
+        head = tail = null;
+    }
+
+    // kiểm tra hàng đợi rỗng
+    public boolean isEmty() {
+        return head == null;
+    }
+
+    // xem thông tin của node head
+    T front() throws Exception {
+        if (isEmty()) {
+            throw new Exception();
+        }
+        return (head.getInfo());
+
+    }
+
+    // xóa node đầu trong queue
+
+    T dequeue() throws Exception {
+        if (isEmty()) {
+            throw new Exception();
+        }
+        T item = head.getInfo();
+        head = head.getNext();
+        if (head == null) {
+            tail = null;
+        }
+
+        return item;
+    }
+
+    // thêm node cuối trong queue
+    void enqueue(T itemT) {
+        if (isEmty()) {
+            head = tail = new Node<T>(null);
+            tail = tail.getNext();
+        }
+    }
+
+}
---
+++ asm2/ASM2/src/asm2/Node.java
@@ -0,0 +1,34 @@
+package asm2;
+
+public class Node<T> {
+    private T info;
+    private Node<T> next;
+
+    public Node() {
+
+    }
+
+    Node(T x) {
+        this.info = x;
+    }
+
+    public Node(T item, Node<T> head) {
+    }
+
+    public T getInfo() {
+        return this.info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public Node<T> getNext() {
+        return this.next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+}
